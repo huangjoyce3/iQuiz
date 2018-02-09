1dcb8720b3e9084369b5d34d7a510ee5fa5d2f55
huangjoyce3 <jhuang3@uw.edu>
2018-02-08 20:33:58 -0800

    basic interation + TableView cells

--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -8,16 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var topicsTable: UITableView!
+    
+    let topics = ["Mathematics":"Are you faster than a calculator?",
+                  "Marvel Super Heros":"How much of a nerd are you?",
+                  "Science":"Only for smartypants."]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        NSLog("numberOfRowsInSection \(Array(topics.keys).count)")
+        return Array(topics.keys).count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        NSLog("We are being asked for indexPath \(indexPath)")
+        let index = indexPath.row
+        let topic = Array(topics.keys)[index]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
+        cell.textLabel?.text = topic
+        cell.detailTextLabel?.text = topics[topic]
+        
+        var image = UIImage(named: "math")!
+        if topic == "Science" {
+            image = UIImage(named: "science")!
+        }
+        if topic == "Marvel Super Heros" {
+            image = UIImage(named: "heros")!
+        }
+        cell.imageView?.image = image
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let topic = Array(topics.keys)[indexPath.row]
+        NSLog("User selected row at \(topic)")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        topicsTable.tableFooterView = UIView()
+        topicsTable.dataSource = self
+        topicsTable.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
