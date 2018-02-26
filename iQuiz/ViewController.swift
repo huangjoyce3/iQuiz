4b49da58eb4c04050412bd141747dd3963056dc3
huangjoyce3 <jhuang3@uw.edu>
2018-02-26 15:53:02 -0800

    part 3 network

--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -15,21 +15,62 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     @IBAction func settingsBtn(_ sender: UIBarButtonItem) {
         let alertVC = UIAlertController(title: "Settings",
-                                        message: "Settings go here",
-            preferredStyle: .alert)
-        alertVC.addAction(UIAlertAction(title: "OK", style: .default) { _ in
+                                        message: "Download quiz info",
+                                        preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "Back", style: .default) { _ in
             alertVC.dismiss(animated: true)
         })
+        let settingsDL = UIAlertAction(title: "Okay", style: .default, handler:{(_ action: UIAlertAction) in self.downloadJSON()
+            if(Network.isConnected()){
+                self.downloadJSON()
+            }else{
+                let alertVC = UIAlertController(title: "Network not available",
+                                                message: "Using local storeage.",
+                                                preferredStyle: .alert)
+                alertVC.addAction(UIAlertAction(title: "Back", style: .default) { _ in
+                    alertVC.dismiss(animated: true)
+                })
+                self.present(alertVC, animated: true)
+            }
+        })
+        alertVC.addAction(settingsDL)
         self.present(alertVC, animated: true)
     }
     
-    let topics = ["Mathematics":"Are you faster than a calculator?",
+    
+    var topics = //[String]()
+        ["Mathematics":"Are you faster than a calculator?",
                   "Marvel Super Heroes":"How much of a nerd are you?",
                   "Science":"Only for smartypants."]
     
+    func downloadJSON(){
+        var dataDict = topics
+        let url = URL(string:"http://tednewardsandbox.site44.com/questions.json")
+        let dl = URLSession(configuration: .default).dataTask(with: url!) {(data, response, error) in do{
+            if let obj = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? NSArray {
+                for topic in obj {
+                    let category = topic as? NSDictionary
+                    if category!["title"] as? NSString == "Marvel Super Heroes"{
+                        self.topics["Marvel Super Heroes"] = category!["desc"] as? NSString as! String
+                    } else if category!["title"] as? NSString == "Science!" {
+                        self.topics["Science"] = category!["desc"] as? NSString as! String
+                    } else{
+                        self.topics["Mathematics"] = category!["desc"] as? NSString as! String
+                    }
+//                    print(category!["title"])
+//                  dataDict.append(category!["title"] as! String)
+                    print(self.topics)
+                }
+            }} catch let error as NSError {
+                print(error.localizedDescription)
+            }
+        }
+        dl.resume()
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        NSLog("numberOfRowsInSection \(Array(topics.keys).count)")
-        return Array(topics.keys).count
+//        NSLog("numberOfRowsInSection \(Array(topics.keys).count)")
+        return 3
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
